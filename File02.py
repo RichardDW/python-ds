beba10879f2c821cfefb8769d24120e33dd5b34f
RichardDW <richard.de.wolff@gmail.com>
2020-06-09 19:37:22 +0200

    Updates

---
+++ File02.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen
+
+
+def fetch_words():
+  # HTTP data is provided as bytes!
+  story = urlopen('http://sixty-north.com/c/t.txt')
+  story_words = []
+  for line in story:
+      line_words = line.decode('utf8').split()
+      for word in line_words:
+          story_words.append(word)
+  story.close()
+  #print(story_words)
+  for word in story_words:
+    print(word)
+
+if __name__ == '__main__':
+  fetch_words()
+
+
+
